9da93ff59f2466ab65bb6dfa974e67ae806f469f
enxb1xd <aldeowy@gmail.com>
2025-02-12 17:51:40 +0300

    css fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,6 @@
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @else
-
-
 </head>
 <body>
 <!-- Первый нав -->
